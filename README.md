31258d2c9f0112b81e61757789fe12cb6c30b292
Raynos <raynos2@gmail.com>
2012-09-22 19:44:24 -0700

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# class-list
+
+A cross browser class list
+
+## Example
+
+## Installation
+
+`npm install class-list`
+
+## Contributors
+
+ - Raynos
+
+## MIT Licenced
---
+++ index.js
@@ -0,0 +1,90 @@
+// contains, add, remove, toggle
+
+module.exports = ClassList
+
+function ClassList(elem) {
+    var cl = elem.classList
+
+    if (cl) {
+        return cl
+    }
+
+    var classList = {
+        add: add
+        , remove: remove
+        , contains: contains
+        , toggle: toggle
+        , toString: $toString
+        , length: 0
+        , item: item
+    }
+
+    return classList
+
+    function add(token) {
+        var list = getTokens()
+        if (list.indexOf(token) > -1) {
+            return
+        }
+        list.push(token)
+        setTokens(list)
+    }
+
+    function remove(token) {
+        var list = getTokens()
+            , index = list.indexOf(token)
+
+        if (index === -1) {
+            return
+        }
+
+        list.splice(index, 1)
+        setTokens(list)
+    }
+
+    function contains(token) {
+        return getTokens().indexOf(token) > -1
+    }
+
+    function toggle(token) {
+        if (contains(token)) {
+            remove(token)
+            return false
+        } else {
+            add(token)
+            return true
+        }
+    }
+
+    function $toString() {
+        return elem.className
+    }
+
+    function item(index) {
+        var tokens = getTokens()
+        return tokens[index] || null
+    }
+
+    function getTokens() {
+        var className = elem.className
+
+        return className.split(" ").filter(isTruthy)
+    }
+
+    function setTokens(list) {
+        var length = list.length
+
+        elem.className = list.join(" ")
+        classList.length = length
+
+        for (var i = 0; i < list.length; i++) {
+            classList[i] = list[i]
+        }
+
+        delete list[length]
+    }
+}
+
+function isTruthy(value) {
+    return !!value
+}
